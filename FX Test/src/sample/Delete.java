d490dac4623e7f55ac99b81c4a2cb8025815dda4
xHollowCellX <48739278+beered239@users.noreply.github.com>
2021-12-23 19:11:56 -0500

    test 3

--- FX Test/src/sample/Delete.java
+++ FX Test/src/sample/Delete.java
@@ -3,6 +3,6 @@ package sample;
 public class Delete {
 	
 	public static void random() {
-		System.out.println("random printed again");
+		System.out.println("random printed again a third time");
 	}
 }
